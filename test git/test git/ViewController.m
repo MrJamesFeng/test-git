2bc6a79dca7fad883871dd6d26c81c3fb06bb007
MrJamesFeng <812463918@qq.co>
2017-03-13 15:53:41 +0800

    初始化

---
+++ test git/test git/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test git
+//
+//  Created by LDY on 17/3/13.
+//  Copyright © 2017年 LDY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
